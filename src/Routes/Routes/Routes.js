12a9bd10ac3c64a288d9de266414d8a38d473c42
tanvirulislam912 <mdtanvir1109040@gmail.com>
2022-10-23 10:59:40 +0500

    first commit

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -20,17 +20,17 @@ import PrivateRoute from "../PrivateRoute/PrivateRoute";
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-ruddy-delta.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-ruddy-delta.vercel.app/category/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-ruddy-delta.vercel.app/news/${params.id}`)
             },
             {
                 path:'/login',
